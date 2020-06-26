932546d7db47e99f29d799fd54d47eeffbed5ce4
Muhammad Asim <64361706+masim111@users.noreply.github.com>
2020-06-26 23:01:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+function getNumber(num){
+//   console.log(num)  
+  var result = document.getElementById("result");
+  result.value += num;
+}
+
+
+function clearResult(){
+    var result = document.getElementById("result");
+    result.value = " "
+}
+function getResult(){
+    var result = document.getElementById("result");
+    // console.log(result.value)
+    result.value = eval(result.value)
+
+}
